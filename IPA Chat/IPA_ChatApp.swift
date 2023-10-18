4391a357900c91dee509e95f389dcc02543e9429
will wade <willwade@gmail.com>
2023-10-18 07:44:18 +0100

    reorganise

---
+++ IPA Chat/IPA_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPA_ChatApp.swift
+//  IPA Chat
+//
+//  Created by Will Wade on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct IPA_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
